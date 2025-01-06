32de66eb720f69d5d8936727ba55641436646998
= <abdoahmedsayed704@gmail.com>
2025-01-06 16:00:32 +0200

    Handle Uploading images

---
+++ middleWares/imageProcessingMiddleware.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const SendError = require("../utils/sendError");
+
+// desc		enable to upload image
+const multerOptions = () => {
+  // memoryStorage
+  const storage = multer.memoryStorage();
+
+  const fileFilteration = function (req, file, cb) {
+    if (file.mimetype.startsWith("image")) {
+      cb(null, true);
+    } else {
+      cb(new SendError("Only images", 400), false);
+    }
+  };
+  const upload = multer({ storage, fileFilter: fileFilteration });
+  return upload;
+};
+
+exports.uploadOneImage = (fieldname) => multerOptions().single(fieldname);
+
+exports.uploadManyImages = (arrayOfFields) =>
+  multerOptions().fields(arrayOfFields);
